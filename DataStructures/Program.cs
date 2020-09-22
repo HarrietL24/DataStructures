591d8f39eb185ed7b6be65cca11fd1aa0c6c3379
Harriet Leplar <15LeplarH@wg6.co.uk>
2020-09-22 14:10:46 +0100

    Changes

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,6 +7,8 @@ namespace DataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
